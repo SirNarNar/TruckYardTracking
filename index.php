84fe6e9b13962f76ca48a9c9fc812ab3b3839bc6
Peter <78226615+SirNarNar@users.noreply.github.com>
2022-04-18 10:03:28 -0400

    Create index.php
    Adding this code in order to make the truck list easier to work with. Currently I have to manually open the file to add or remove a truck. Adding this makes it possible to change it from different computers and well as for anyone else to add or remove a truck from the list.

---
+++ index.php
@@ -0,0 +1,35 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="pragma" content="no-cache" />
+</head>
+<body>
+<fieldset class="fieldset">
+    <legend><h1>Trucks Near Yard Email List</h1></legend>
+    <section>
+    <h2>Current List:</h2>
+    <object width="250" height="50" type="text/plain" data="Trucks.txt" ></object>
+    <form action="EditTrucks.php" method="post">
+        <table>
+            <tr>
+                <td>Truck:</td>
+                <td><input type="text" name="truckName"></td>
+            </tr>
+            <tr>
+                <td>Type:</td>
+                <td><input type="radio" id="add" name="type" value="add" checked><label for="add" class="green">&nbspADD</label></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="radio" id="remove" name="type" value="remove"><label for="remove" class="red">&nbspREMOVE</label></td>
+            </tr>
+            <tr>
+                <td><input type="submit"></td>
+            </tr>
+        </table>
+    </form>
+</section>
+    <code>If the list above does not update, press Ctrl+F5 to refresh and clear your cache.</code><br>
+    <code>Code is located at C:\Users\Staff\Downloads\Xampp\htdocs\scripts\WorkingCode\ on ......</code>
+</fieldset>
+<script>
+</body>
